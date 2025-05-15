f8c84ddfb0d69b8520b8ccdf2611c758edb4be2a
Cristina <kamuthucristina@gmail.com>
2025-05-15 22:38:56 +0300

    completed the music player section

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const base_url = "http://localhost:3000/songs";
 fetch(base_url)
   .then(res => res.json())
   .then(songs => list_of_songs(songs))
-  .catch(err => console.log(err || "Cristina Kuna error!"));
+  .catch(err => console.log(err && "Cristina Kuna error!"));
 
 function list_of_songs(songs) {
   const songsList = document.getElementById("my_songlist");
@@ -17,7 +17,7 @@ function list_of_songs(songs) {
           <source src="${song.url}" type="audio/mpeg">
           Sadly, umebunt kimuziki
         </audio>
-        <button data-id="${song.id}>Like</button>
+        <button id="${song.id}">Like</button>
       </li>
     `;
     songsList.innerHTML += html;
