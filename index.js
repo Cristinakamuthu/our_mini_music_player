4222b8c59af26c68959d1019ee65076018f5b809
Cristina <kamuthucristina@gmail.com>
2025-05-16 10:17:26 +0300

    added search bar

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const base_url = "http://localhost:3000/songs";
 
+let allsongs = []
 fetch(base_url)
   .then(res => res.json())
-  .then(songs => list_of_songs(songs))
+  .then(songs => {allsongs = songs; list_of_songs(songs)})
   .catch(err => console.log(err && "Cristina Kuna error!"));
+
+document.addEventListener("DOMContentLoaded",() =>{
+    const searchinput = document.getElementById("searchbar")
+
+    searchinput.addEventListener("input",()=>{
+        const searchstuff = searchinput.value.toLowerCase();
+
+        const filteredSongs = allsongs.filter(song => song.title.toLowerCase().includes(searchstuff))
+
+        list_of_songs(filteredSongs)
+    })
+})
+
+
+
+
+
+
 
 function list_of_songs(songs) {
   const songsList = document.getElementById("my_songlist");
